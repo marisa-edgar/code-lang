aedf65db1b8d3c0af7cc4e1f9f9c5ff0ef3eac4e
marisa-edgar <marisaedgar1212@gmail.com>
2022-01-14 12:06:43 -0800

    create scripts for each form

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+  let total= 0;
+  $("#form").submit(function(event){
+    const name =$("name").val();
+    event.preventDefault();
+    $(".page").hide();
+    $(".page1").show();
+    $(".insertName").text(name);
+  });
+  
+  $("#form1").submit(function(event){
+    const name =$("name").val();
+    const page1 = $("input:radio[name=page1]").val();
+    event.prventDefault();
+    $(".page1").hide();
+    $(".page2").dhow();
+    $(".insertName").text(name);
+  });
+
+  $("#form2").submit(function(event){
+    const name =$("name").val();
+    const page2 = $("input:radio[name=page2]").val();
+    event.preventDefault();
+    $(".page2").hide();
+    $(".page3").dhow();
+    $(".insertName").text(name);
+  });
+
+  $("#form3").submit(function(event){
+    const name =$("name").val();
+    const page3 = $("input:radio[name=page3]").val();
+    event.preventDefault();
+    $(".page3").hide();
+    $(".page4").dhow();
+    $(".insertName").text(name);
+  });
+
+  $("#form4").submit(function(event){
+    const name =$("name").val();
+    const page4 = $("input:radio[name=page4]").val();
+    event.preventDefault();
+    $(".page4").hide();
+    $(".page5").dhow();
+    $(".insertName").text(name);
+  });
+
+  $("#form5").submit(function(event){
+    const name =$("name").val();
+    const page5 = $("input:radio[name=page5]").val();
+    event.preventDefault();
+    $(".page5").hide();
+    $(".page6").dhow();
+    $(".insertName").text(name);
+  });
+});
