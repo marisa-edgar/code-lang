b656863f6e305abb0840204f898906c36fe3b4bf
marisa-edgar <marisaedgar1212@gmail.com>
2022-01-17 19:30:27 -0800

    add final questions

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
   let total= 0;
   $("#form").submit(function(event){
     const name =$("name").val();
-    console.log("It Worked!")
     event.preventDefault();
     $(".page").hide();
     $(".page1").show();
@@ -11,7 +10,6 @@ $(document).ready(function(){
   
   $("#form1").submit(function(event){
     const page1 = $("input:radio[name=page1]:checked").val();
-    console.log("It Worked!")
     event.preventDefault();
     $(".page1").hide();
     if (page1 === "1") {
@@ -25,7 +23,6 @@ $(document).ready(function(){
 
   $("#form2-1").submit(function(event){
     const twoOne = $("input:radio[name=page2-1]:checked").val();
-    console.log("It Worked!")
     event.preventDefault();
     $(".page2-1").hide();
     if (twoOne === "1") {
@@ -65,16 +62,14 @@ $(document).ready(function(){
 
   $("#form3-2").submit(function(event){
     const threeTwo = $("input:radio[name=page3-2]:checked").val();
-    console.log("It Worked!")
     event.preventDefault();
     $(".page3-2").hide();
     if (threeTwo === "1") {
       total ++;
-      $(".page4-3").show();
+      $(".page4-2").show();
     } else if (threeTwo === "2") {
       total += 2;
-      $(".page4-4").show();
-      console.log("It Worked!")
+      $(".page4-2").show();
     };
   });
 
@@ -84,10 +79,10 @@ $(document).ready(function(){
     $(".page3-3").hide();
     if (threeThree === "1") {
       total ++;
-      $(".page4-5").show();
+      $(".page4-3").show();
     } else if (threeThree === "2") {
       total += 2;
-      $(".page4-6").show();
+      $(".page4-3").show();
     };
   });
 
@@ -113,7 +108,7 @@ $(document).ready(function(){
       $(".page5-1").show();
     } else if (fourOne === "2") {
       total += 2;
-      $(".page5-2").show();
+      $(".page5-1").show();
     };
   });
 
@@ -123,89 +118,90 @@ $(document).ready(function(){
     $(".page4-2").hide();
     if (fourTwo === "1") {
       total ++;
-      $(".page5-3").show();
+      $(".page5-2").show();
     } else if (fourTwo === "2") {
+      total += 2;
+      $(".page5-2").show();
+    };
+  });
+
+  $("#form4.3").submit(function(event){
+    const fourThree = $("input:radio[name=page4.3]:checked").val();
+    event.preventDefault();
+    $(".page4-3").hide();
+    if (fourThree === "1") {
+      total ++;
+      $(".page5-3").show();
+    } else if (fourThree === "2") {
+      total += 2;
+      $(".page5-3").show();
+    };
+  });
+
+  $("#form4.4").submit(function(event){
+    const fourFour = $("input:radio[name=page4.4]:checked").val();
+    event.preventDefault();
+    $(".page4-4").hide();
+    if (fourFour === "1") {
+      total ++;
+      $(".page5-4").show();
+    } else if (fourFour === "2") {
       total += 2;
       $(".page5-4").show();
     };
   });
 
-  // $("#form4.3").submit(function(event){
-  //   const threeThree = $("input:radio[name=page3.3]:checked").val();
-  //   event.preventDefault();
-  //   $(".page3-3").hide();
-  //   if (threeThree === "1") {
-  //     total ++;
-  //     $(".page5-5").show();
-  //   } else if (threeThree === "2") {
-  //     total += 2;
-  //     $(".page5-6").show();
-  //   };
-  // });
 
-  // $("#form4.4").submit(function(event){
-  //   const threeFour = $("input:radio[name=page3.4]:checked").val();
-  //   event.preventDefault();
-  //   $(".page3-4").hide();
-  //   if (threeFour === "1") {
-  //     total ++;
-  //     $(".page5-7").show();
-  //   } else if (threeFour === "2") {
-  //     total += 2;
-  //     $(".page5-8").show();
-  //   };
-  // });
+  $("#form5.1").submit(function(event){
+    const fiveOne = $("input:radio[name=page5.1]:checked").val();
+    event.preventDefault();
+    $(".page5-1").hide();
+    if (fiveOne === "1") {
+      total ++;
+      $(".page6-1").show();
+    } else if (fiveOne === "2") {
+      total += 2;
+      $(".page6-1").show();
+    };
+  });
 
-  // $("#form4.5").submit(function(event){
-  //   const threeOne = $("input:radio[name=page3.1]:checked").val();
-  //   event.preventDefault();
-  //   $(".page3-1").hide();
-  //   if (threeOne === "1") {
-  //     total ++;
-  //     $(".page4-1").show();
-  //   } else {
-  //     total += 2;
-  //     $(".page4-2").show();
-  //   };
-  // });
+  $("#form5.2").submit(function(event){
+    const fiveTwo = $("input:radio[name=page5.2]:checked").val();
+    event.preventDefault();
+    $(".page5-2").hide();
+    if (fiveTwo === "1") {
+      total ++;
+      $(".page6-2").show();
+    } else if (fiveTwo === "2") {
+      total += 2;
+      $(".page6-2").show();
+    };
+  });
 
-  // $("#form4.6").submit(function(event){
-  //   const threeTwo = $("input:radio[name=page3.2]:checked").val();
-  //   event.preventDefault();
-  //   $(".page3-2").hide();
-  //   if (threeTwo === "1") {
-  //     total ++;
-  //     $(".page4-3").show();
-  //   } else if (threeTwo === "2") {
-  //     total += 2;
-  //     $(".page4-4").show();
-  //   };
-  // });
+  $("#form5.3").submit(function(event){
+    const fiveThree = $("input:radio[name=page5.3]:checked").val();
+    event.preventDefault();
+    $(".page5-3").hide();
+    if (fiveThree === "1") {
+      total ++;
+      $(".page6-3").show();
+    } else if (fiveThree === "2") {
+      total += 2;
+      $(".page6-3").show();
+    };
+  });
 
-  // $("#form4.7").submit(function(event){
-  //   const threeThree = $("input:radio[name=page3.3]:checked").val();
-  //   event.preventDefault();
-  //   $(".page3-3").hide();
-  //   if (threeThree === "1") {
-  //     total ++;
-  //     $(".page4-5").show();
-  //   } else if (threeThree === "2") {
-  //     total += 2;
-  //     $(".page4-6").show();
-  //   };
-  // });
-
-  // $("#form4.8").submit(function(event){
-  //   const threeFour = $("input:radio[name=page3.4]:checked").val();
-  //   event.preventDefault();
-  //   $(".page3-4").hide();
-  //   if (threeFour === "1") {
-  //     total ++;
-  //     $(".page4-7").show();
-  //   } else if (threeFour === "2") {
-  //     total += 2;
-  //     $(".page4-8").show();
-  //   };
-  // });
+  $("#form5.4").submit(function(event){
+    const fiveFour = $("input:radio[name=page5.4]:checked").val();
+    event.preventDefault();
+    $(".page5-4").hide();
+    if (fiveFour === "1") {
+      total ++;
+      $(".page6-4").show();
+    } else if (fiveFour === "2") {
+      total += 2;
+      $(".page6-4").show();
+    };
+  });
 
 });
